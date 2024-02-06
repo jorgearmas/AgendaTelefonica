8fb5596ce7c15d91d98ae6f1e95c6285273983d8
jarmas <jorgearmasc0496@gmail.com>
2024-02-06 13:17:58 -0600

    Logic layer only initializes and deploys gui

---
+++ AgendaTelefonica/src/agendatelefonica/AgendaTelefonica.java
@@ -0,0 +1,15 @@
+
+package agendatelefonica;
+import gui.Principal;
+
+public class AgendaTelefonica {
+
+    public static void main(String[] args) {
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+    
+}
